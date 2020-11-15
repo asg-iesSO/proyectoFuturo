e02aabaa10af9bc0cde37f6aface8ac71e6db5ba
Ariadna <ariadnasn1@hotmail.com>
2020-11-15 21:10:19 +0100

    First commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Estudiante " + args[0]);
+        System.out.println(veredicto(args[1],args[2]));
+
+    }
+
+    public static String veredicto(String valor1, String valor2) {
+        String veredictoFinal = "";
+        if (valor1.equals(valor2)) {
+            veredictoFinal = "Bien. Pero una hora de estudio para cada módulo puede ser insuficiente.";
+        } else if (Integer.valueOf(valor1) < Integer.valueOf(valor2)) {
+            veredictoFinal = "Poco tiempo de estudio. Debes dedicar más tiempo.";
+        } else {
+            veredictoFinal = "Ideal. Trabajas los contenidos en casa.";
+        }
+        return veredictoFinal;
+    }
+}
